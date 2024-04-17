715c1b59cf51084fbea7bb1a835bed84c1930781
mchavez <miguel_chavez_m@hotmail.com>
2024-04-17 10:37:17 -0600

    Add OAuth 2.0 With Private Key (#23)
    * OAuth 2.0 With Private Key
    
    * Rename variable
    
    * Add validations
    
    * Quick change
    
    * Setting scopes
    
    * Adding provisioning flag
    
    * Refactoring code
    
    * Update readme
    
    * Code review changes
    
    * Code review changes
    
    * Code review changes
    
    * Code review changes
    
    * Code review changes
    
    * Quick change
    
    * Quick change
    
    * Code review changes
    
    * Code review changes
    
    * Code review changes
    
    * Add validation
    
    * Code review changes

--- cmd/baton-okta/config.go
+++ cmd/baton-okta/config.go
@@ -14,7 +14,11 @@ type config struct {
 
 	Domain           string `mapstructure:"domain"`
 	ApiToken         string `mapstructure:"api-token"`
+	OktaClientId     string `mapstructure:"okta-client-id"`
+	OktaPrivateKey   string `mapstructure:"okta-private-key"`
+	OktaPrivateKeyId string `mapstructure:"okta-private-key-id"`
 	SyncInactiveApps bool   `mapstructure:"sync-inactive-apps"`
+	OktaProvisioning bool   `mapstructure:"provisioning"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
@@ -24,7 +28,19 @@ func validateConfig(ctx context.Context, cfg *config) error {
 	}
 
 	if cfg.ApiToken == "" {
-		return fmt.Errorf("api token is missing")
+		if cfg.OktaClientId == "" {
+			return fmt.Errorf("either api token or client id is required")
+		} else if cfg.OktaClientId != "" && cfg.OktaPrivateKey == "" || cfg.OktaPrivateKeyId == "" {
+			return fmt.Errorf("private key and private key id required")
+		}
+
+		if cfg.OktaClientId == "" && cfg.OktaPrivateKey == "" && cfg.OktaPrivateKeyId == "" {
+			return fmt.Errorf("client id, private key and private key id required")
+		}
+	}
+
+	if cfg.ApiToken != "" && cfg.OktaClientId != "" {
+		return fmt.Errorf("api token and client id cannot be provided simultaneously")
 	}
 
 	return nil
@@ -33,6 +49,9 @@ func validateConfig(ctx context.Context, cfg *config) error {
 // cmdFlags sets the cmdFlags required for the connector.
 func cmdFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().String("domain", "", "The URL for the Okta organization. ($BATON_DOMAIN)")
+	cmd.PersistentFlags().String("okta-client-id", "", "The Okta Client ID. ($BATON_OKTA_CLIENT_ID)")
+	cmd.PersistentFlags().String("okta-private-key", "", "The Okta Private Key. This can be the whole private key or the path to the private key. ($BATON_OKTA_PRIVATE_KEY)")
+	cmd.PersistentFlags().String("okta-private-key-id", "", "The Okta Private Key ID. ($BATON_OKTA_PRIVATE_KEY_ID)")
 	cmd.PersistentFlags().String("api-token", "", "The API token for the service account.  ($BATON_API_TOKEN)")
 	cmd.PersistentFlags().Bool("sync-inactive-apps", true, "Whether to sync inactive apps or not.  ($BATON_SYNC_INACTIVE_APPS)")
 }
--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -9,9 +9,7 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/uhttp"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"github.com/okta/okta-sdk-golang/v2/okta"
-	"go.uber.org/zap"
 
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 )
@@ -21,6 +19,14 @@ type Okta struct {
 	domain           string
 	apiToken         string
 	syncInactiveApps bool
+	config           *ConfigOAuth2
+}
+
+type ConfigOAuth2 struct {
+	OktaClientId            string
+	OktaPrivateKey          string
+	OktaPrivateKeyId        string
+	OktaProvisioningEnabled bool
 }
 
 func v1AnnotationsForResourceType(resourceTypeID string, skipEntitlementsAndGrants bool) annotations.Annotations {
@@ -60,6 +66,18 @@ var (
 		DisplayName: "App",
 		Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_APP},
 		Annotations: v1AnnotationsForResourceType("app", false),
+	}
+	defaultScopes = []string{
+		"okta.users.read",
+		"okta.groups.read",
+		"okta.roles.read",
+		"okta.apps.read",
+	}
+	provisioningScopes = []string{
+		"okta.users.manage",
+		"okta.groups.manage",
+		"okta.roles.manage",
+		"okta.apps.manage",
 	}
 )
 
@@ -102,24 +120,25 @@ func (c *Okta) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
 }
 
 func (c *Okta) Validate(ctx context.Context) (annotations.Annotations, error) {
-	l := ctxzap.Extract(ctx)
-	token := newPaginationToken(defaultLimit, "")
+	if c.apiToken != "" {
+		token := newPaginationToken(defaultLimit, "")
 
-	_, respCtx, err := getOrgSettings(ctx, c.client, token)
-	if err != nil {
-		return nil, fmt.Errorf("okta-connector: verify failed to fetch org: %w", err)
+		_, respCtx, err := getOrgSettings(ctx, c.client, token)
+		if err != nil {
+			return nil, fmt.Errorf("okta-connector: verify failed to fetch org: %w", err)
+		}
+
+		_, _, err = parseResp(respCtx.OktaResponse)
+		if err != nil {
+			return nil, fmt.Errorf("okta-connector: verify failed to parse response: %w", err)
+		}
+
+		if respCtx.OktaResponse.StatusCode != http.StatusOK {
+			err := fmt.Errorf("okta-connector: verify returned non-200: '%d'", respCtx.OktaResponse.StatusCode)
+			return nil, err
+		}
 	}
 
-	_, _, err = parseResp(respCtx.OktaResponse)
-	if err != nil {
-		return nil, fmt.Errorf("okta-connector: verify failed to parse response: %w", err)
-	}
-
-	if respCtx.OktaResponse.StatusCode != http.StatusOK {
-		err := fmt.Errorf("okta-connector: verify returned non-200: '%d'", respCtx.OktaResponse.StatusCode)
-		l.Error("Invalid Status Code from Verity", zap.Error(err))
-		return nil, err
-	}
 	return nil, nil
 }
 
@@ -127,26 +146,65 @@ func (c *Okta) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCl
 	return "", nil, fmt.Errorf("not implemented")
 }
 
-func New(ctx context.Context, domain, apiToken string, syncInactiveApps bool) (*Okta, error) {
+func NewOktaClient(domain, apiToken string, syncInactiveApps bool, cfg *ConfigOAuth2) *Okta {
+	return &Okta{
+		client:           &okta.Client{},
+		domain:           domain,
+		apiToken:         apiToken,
+		syncInactiveApps: syncInactiveApps,
+		config: &ConfigOAuth2{
+			OktaClientId:            cfg.OktaClientId,
+			OktaPrivateKey:          cfg.OktaPrivateKey,
+			OktaPrivateKeyId:        cfg.OktaPrivateKeyId,
+			OktaProvisioningEnabled: cfg.OktaProvisioningEnabled,
+		},
+	}
+}
+
+func New(ctx context.Context, o *Okta) (*Okta, error) {
+	var (
+		oktaClient *okta.Client
+		scopes     = defaultScopes
+	)
 	client, err := uhttp.NewClient(ctx, uhttp.WithLogger(true, nil))
 	if err != nil {
 		return nil, err
 	}
 
-	_, oktaClient, err := okta.NewClient(ctx,
-		okta.WithOrgUrl(fmt.Sprintf("https://%s", domain)),
-		okta.WithToken(apiToken),
-		okta.WithHttpClientPtr(client),
-		okta.WithCache(false),
-	)
-	if err != nil {
-		return nil, err
+	if o.apiToken != "" && o.domain != "" {
+		_, oktaClient, err = okta.NewClient(ctx,
+			okta.WithOrgUrl(fmt.Sprintf("https://%s", o.domain)),
+			okta.WithToken(o.apiToken),
+			okta.WithHttpClientPtr(client),
+			okta.WithCache(false),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if o.config.OktaClientId != "" && o.config.OktaPrivateKey != "" && o.domain != "" {
+		if o.config.OktaProvisioningEnabled {
+			scopes = append(scopes, provisioningScopes...)
+		}
+		_, oktaClient, err = okta.NewClient(ctx,
+			okta.WithOrgUrl(fmt.Sprintf("https://%s", o.domain)),
+			okta.WithAuthorizationMode("PrivateKey"),
+			okta.WithClientId(o.config.OktaClientId),
+			okta.WithScopes(scopes),
+			okta.WithPrivateKey(o.config.OktaPrivateKey),
+			okta.WithPrivateKeyId(o.config.OktaPrivateKeyId),
+			okta.WithCache(false),
+		)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &Okta{
 		client:           oktaClient,
-		domain:           domain,
-		apiToken:         apiToken,
-		syncInactiveApps: syncInactiveApps,
+		domain:           o.domain,
+		apiToken:         o.apiToken,
+		syncInactiveApps: o.syncInactiveApps,
 	}, nil
 }
