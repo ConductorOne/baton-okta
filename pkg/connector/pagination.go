288256dea412f28f2ba35f05004c505ef4b6b47d
laurenleach <106626058+laurenleach@users.noreply.github.com>
2024-11-19 17:59:11 -0800

    Increase default pagination size to 500 (#56)

--- pkg/connector/pagination.go
+++ pkg/connector/pagination.go
@@ -9,7 +9,7 @@ import (
 	"github.com/okta/okta-sdk-golang/v2/okta"
 )
 
-const defaultLimit = 100
+const defaultLimit = 500
 
 func parseGetResp(resp *okta.Response) (annotations.Annotations, error) {
 	var annos annotations.Annotations
