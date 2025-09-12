f966e7be82dafbe0475bea624503d152451004be
MJ Palanker <mj.palanker@conductorone.com>
2025-09-12 13:55:33 -0700

    integrate not found eror handling

--- pkg/connector/user.go
+++ pkg/connector/user.go
@@ -716,8 +716,7 @@ func getUser(ctx context.Context, client *okta.Client, oktaUserID string) (*okta
 
 	resp, err := rq.Do(ctx, req, &oktaUsers)
 	if err != nil {
-		err = convertNotFoundError(err, "user not found")
-		return nil, nil, err
+		return nil, nil, handleOktaResponseErrorWithNotFoundMessage(resp, err, "user not found")
 	}
 
 	return oktaUsers, &responseContext{OktaResponse: resp}, nil
